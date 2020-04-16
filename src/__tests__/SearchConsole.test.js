6dec3846a1dc4e9ab82406842617de865af79a63
edsonha <edsonha@gmail.com>
2020-04-16 23:25:44 +0800

    [Ed] Add test for onchange event on the input field

--- src/__tests__/SearchConsole.test.js
+++ src/__tests__/SearchConsole.test.js
@@ -1,6 +1,15 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import SearchConsole from "../components/SearchConsole/SearchConsole";
+
+global.document.createRange = () => ({
+  setStart: () => {},
+  setEnd: () => {},
+  commonAncestorContainer: {
+    nodeName: "BODY",
+    ownerDocument: document,
+  },
+});
 
 describe("Search Console", () => {
   it("should render the input textbox", () => {
@@ -11,5 +20,22 @@ describe("Search Console", () => {
     const inputBox = getByLabelText("Search Github Repository");
 
     expect(inputBox).toBeInTheDocument();
+  });
+
+  it("should trigger the onChange function when the input text is filled in", () => {
+    const handleChange = jest.fn();
+
+    const { getByLabelText } = render(
+      <SearchConsole
+        handleChange={handleChange}
+        inputOptions={["value1", "value2"]}
+      />
+    );
+
+    const inputBox = getByLabelText("Search Github Repository");
+
+    fireEvent.change(inputBox, { target: { value: "react" } });
+
+    expect(inputBox).toHaveAttribute("value", "react");
   });
 });
